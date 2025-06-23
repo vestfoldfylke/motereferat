e2505e4c6fdc7e823c6e6664f7cd5b1c80205b69
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2025-06-23 11:51:56 +0200

    feat: Update meeting queue tests to include new meeting configuration and mock data

--- tests/smart-meetings.test.js
+++ tests/smart-meetings.test.js
@@ -1,7 +1,37 @@
 import { describe, it } from 'node:test'
 import assert from 'node:assert'
-import { createMeetingQueue, getMeetingDate } from '../lib/smart/smart-meetings.js'
+import { createMeetingQueue, getMeetingDate, SmartMeeting } from '../lib/smart/smart-meetings.js'
 import { createSimpleCache } from '../lib/simple-cache.js'
+import { ListInfo } from '../lib/graph.js'
+import { MeetingConfig } from '../motereferat-config/sakslister.js'
+import { createDefaultArchiveFlowStatus } from '../lib/smart/archive-flow-status.js'
+
+const createMockMeetingItem = (id, title, meetingDate) => {
+  /** @type {import('../lib/smart/smart-meeting-items.js').SmartMeetingItem} */
+  const item = {
+    id,
+    title,
+    meetingDate,
+    archiveStatus: 'Bubu',
+    itemType: 'Beslutning',
+    itemStatus: 'Avsluttet',
+    itemResponsibleName: 'Test Ansvarlig',
+    description: 'Huhuhu',
+    descriptionText: 'Test beskrivelse',
+    decision: 'Test vedtak',
+    decisionText: 'Test vedtak',
+    documentNumber: 'Ikke arkivert',
+    elementVersion: 1,
+    hasAttachments: false,
+    itemResponsibleLookupId: null,
+    minutesId: 0,
+    publishAttachments: 'Nei',
+    publishItem: 'Ja',
+    sorting: 100,
+    reArchive: 'Nei'
+  }
+  return item
+}
 
 describe('getMeetingDate works as expected when', () => {
   it('has a valid date', () => {
@@ -25,64 +55,67 @@ describe('getMeetingDate works as expected when', () => {
 })
 
 describe('createMeetingQueue works as expected when', () => {
-  const mockedListInfo = {
+  const mockedListInfo = ListInfo.parse({
     siteId: 'enSite',
     listId: 'enListe',
     siteName: 'EnSiteDa',
-    listName: 'EnListeDa'
-  }
+    listName: 'EnListeDa',
+    listUrl: 'https://example.com/sites/EnSiteDa/EnListeDa',
+    listDisplayName: 'En Liste Da',
+  })
+  const mockedMeetingConfig = MeetingConfig.parse({
+    MEETING_ARENA: 'En Møte Arena',
+    LIST_URL: 'https://example.com/sites/EnSiteDa/EnListeDa',
+    ARCHIVE: {
+      CASE_EXTERNAL_ID_PREFIX: 'ORG-SMARTMtereferat-test',
+      RESPONSIBLE_ENTERPRISE_RECNO: 123
+    },
+    PDF: {
+      SECTOR: 'Test seksjon'
+    }
+  })
+
   const meetingCache = createSimpleCache()
 
   /** @type {import('../lib/smart/smart-meeting-items.js').SmartMeetingItem[]} */
   const newMeetingItems = [
-    {
-      id: '1',
-      title: 'Meeting Item 1',
-      meetingDate: '2023-10-01T22:00:00Z'
-    },
-    {
-      id: '2',
-      title: 'Meeting Item 2',
-      meetingDate: '2023-10-01T22:00:00Z'
-    },
-    {
-      id: '3',
-      title: 'Meeting Item 3',
-      meetingDate: '2023-10-02T23:00:00Z'
-    }
+    createMockMeetingItem('1', 'Meeting Item 1', '2023-10-01T22:00:00Z'),
+    createMockMeetingItem('2', 'Meeting Item 2', '2023-10-01T22:00:00Z'),
+    createMockMeetingItem('3', 'Meeting Item 3', '2023-10-02T23:00:00Z')
   ]
   it('it has no cached meetings', () => {
     meetingCache.clear()
-    const queue = createMeetingQueue(mockedListInfo, meetingCache, newMeetingItems)
+    const queue = createMeetingQueue(mockedMeetingConfig, mockedListInfo, meetingCache, newMeetingItems)
     assert.strictEqual(queue.length, 2)
-    assert.strictEqual(queue[0].meetingId, 'EnSiteDa-EnListeDa-2023-10-01')
-    assert.strictEqual(queue[0].meetingDate, '2023-10-01')
+    assert.strictEqual(queue[0].meetingId, 'EnSiteDa-EnListeDa-2023-10-02')
+    assert.strictEqual(queue[0].meetingDate, '2023-10-02')
     assert.strictEqual(queue[0].items.length, 2)
-    assert.strictEqual(queue[1].meetingId, 'EnSiteDa-EnListeDa-2023-10-02')
-    assert.strictEqual(queue[1].meetingDate, '2023-10-02')
+    assert.strictEqual(queue[1].meetingId, 'EnSiteDa-EnListeDa-2023-10-03')
+    assert.strictEqual(queue[1].meetingDate, '2023-10-03')
     assert.strictEqual(queue[1].items.length, 1)
   })
   it('it has one cached meetings, which is also in newMeetingItems - should use cached meeting', () => {
     meetingCache.clear()
-    const cachedMeeting = {
-      meetingId: 'EnSiteDa-EnListeDa-2023-10-01',
-      meetingDate: '2023-10-01',
+    /** @type {SmartMeeting} */
+    const cachedMeeting = {      
+      meetingId: 'EnSiteDa-EnListeDa-2023-10-02',
+      meetingDate: '2023-10-02',
+      archiveFlowStatus: createDefaultArchiveFlowStatus(),
+      listInfo: mockedListInfo,
+      meetingConfig: mockedMeetingConfig,
+      queuedDate: new Date().toISOString(),
       items: [
-        {
-          id: 'cached-item',
-          title: 'Cached Item',
-          meetingDate: '2023-10-01T22:00:00Z'
-        }
+        createMockMeetingItem('cached-item', 'Cached Item', '2023-10-01T22:00:00Z')
       ]
     }
-    meetingCache.set('EnSiteDa-EnListeDa-2023-10-01', cachedMeeting)
-    const queue = createMeetingQueue(mockedListInfo, meetingCache, newMeetingItems)
+    meetingCache.set('EnSiteDa-EnListeDa-2023-10-02', cachedMeeting)
+    const queue = createMeetingQueue(mockedMeetingConfig, mockedListInfo, meetingCache, newMeetingItems)
     assert.strictEqual(queue.length, 2)
-    assert.strictEqual(queue[0].meetingId, 'EnSiteDa-EnListeDa-2023-10-01')
+    assert.strictEqual(queue[0].meetingId, 'EnSiteDa-EnListeDa-2023-10-02')
     assert.strictEqual(queue[0].items.length, 1) // 2 new items in new meeting, but 1 in cached item - we prefere cached item
     assert.strictEqual(queue[0].items[0].id, 'cached-item')
-    assert.strictEqual(queue[1].meetingId, 'EnSiteDa-EnListeDa-2023-10-02')
-    assert.strictEqual(queue[1].meetingDate, '2023-10-02')
+    assert.strictEqual(queue[1].meetingId, 'EnSiteDa-EnListeDa-2023-10-03')
+    assert.strictEqual(queue[1].meetingDate, '2023-10-03')
     assert.strictEqual(queue[1].items.length, 1)
   })
   it('it has two cached meetings, which is not in newMeetingItems - the one corresponding to the listid and siteid should be included in the cached meeting first in the queue, the other not be included at all', () => {
@@ -90,44 +123,36 @@ describe('createMeetingQueue works as expected when', () => {
     const cachedMeeting = {
       meetingId: 'EnSiteDa-EnListeDa-2023-10-06',
       meetingDate: '2023-10-06',
+      archiveFlowStatus: createDefaultArchiveFlowStatus(),
+      listInfo: mockedListInfo,
+      meetingConfig: mockedMeetingConfig,
+      queuedDate: new Date().toISOString(),
       items: [
-        {
-          id: 'cached-item',
-          title: 'Cached Item',
-          meetingDate: '2023-10-06T22:00:00Z'
-        },
-        {
-          id: 'cached-item-2',
-          title: 'Cached Item 2',
-          meetingDate: '2023-10-06T22:00:00Z'
-        },
-        {
-          id: 'cached-item-3',
-          title: 'Cached Item 3',
-          meetingDate: '2023-10-06T22:00:00Z'
-        }
+        createMockMeetingItem('cached-item', 'Cached Item', '2023-10-05T22:00:00Z'),
+        createMockMeetingItem('cached-item-2', 'Cached Item 2', '2023-10-05T22:00:00Z'),
+        createMockMeetingItem('cached-item-3', 'Cached Item 3', '2023-10-05T22:00:00Z')
       ]
     }
     const anotherIrrelevantCachedMeeting = {
       meetingId: 'EnAnnenSiteDa-EnListeDa-2023-10-05',
       meetingDate: '2023-10-05',
+      archiveFlowStatus: createDefaultArchiveFlowStatus(),
+      listInfo: mockedListInfo,
+      meetingConfig: mockedMeetingConfig,
+      queuedDate: new Date().toISOString(),
       items: [
-        {
-          id: 'cached-item-4',
-          title: 'Cached Item 4',
-          meetingDate: '2023-10-05T22:00:00Z'
-        }
+        createMockMeetingItem('cached-item-4', 'Cached Item 4', '2023-10-05T00:00:00Z')
       ]
     }
     meetingCache.set('EnSiteDa-EnListeDa-2023-10-06', cachedMeeting)
     meetingCache.set('EnAnnenSiteDa-EnListeDa-2023-10-05', anotherIrrelevantCachedMeeting)
-    const queue = createMeetingQueue(mockedListInfo, meetingCache, newMeetingItems)
+    const queue = createMeetingQueue(mockedMeetingConfig, mockedListInfo, meetingCache, newMeetingItems)
     assert.strictEqual(queue.length, 3)
     assert.strictEqual(queue[0].meetingId, 'EnSiteDa-EnListeDa-2023-10-06')
     assert.strictEqual(queue[0].meetingDate, '2023-10-06')
     assert.strictEqual(queue[0].items.length, 3)
     assert.strictEqual(queue[0].items[0].id, 'cached-item')
-    assert.strictEqual(queue[1].meetingId, 'EnSiteDa-EnListeDa-2023-10-01')
+    assert.strictEqual(queue[1].meetingId, 'EnSiteDa-EnListeDa-2023-10-02')
     assert.strictEqual(queue.some(meeting => meeting.meetingId === 'EnAnnenSiteDa-EnListeDa-2023-10-05'), false)
   })
 })
